6347fc5f0601bae80e85a751546d72be90f74d84
hofemar <office@mario-hofer.biz>
2014-04-18 23:11:46 +0200

    Addes some further comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,13 +6,19 @@
 ## matrix is assigned the cache gets reset.
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
+    ## Set function for matrix (assigns new matrix)
     set <- function(y) {
         x <<- y
         inv <<- NULL
     }
+    ## Get function for matrix (retrieves current matrix)
     get <- function() x
+    
+    ## Set and get functions for cache
     setinv <- function(solve) inv <<- solve
     getinv <- function() inv
+    
+    ## return list of functions
     list(set = set, get = get,
          setinv = setinv,
          getinv = getinv)
@@ -25,12 +31,18 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     inv <- x$getinv()
+    
+    ## Check if cached results already exist
     if(!is.null(inv)) {
         message("getting cached data")
         return(inv)
     }
     data <- x$get()
+    
+    ## Compute inverse
     inv <- solve(data, ...)
+    
+    ## Write inverse to cache
     x$setinv(inv)
     inv
 }
